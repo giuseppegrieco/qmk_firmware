58904d9595502090bf21c55deefc8a4ef46d8e05
Giuseppe Grieco <g.grieco1997@gmail.com>
2026-05-25 15:35:35 +0200

    add print key

--- keyboards/handwired/dactyl_manuform/5x7/keymaps/giuseppeg/keymap.c
+++ keyboards/handwired/dactyl_manuform/5x7/keymaps/giuseppeg/keymap.c
@@ -16,7 +16,7 @@
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /*
      * ,--------------------------------------------------.           ,--------------------------------------------------.
-     * |   ESC  |   1  |   2  |   3  |   4  |   5  | PGUP |           | PGDN |   6  |   7  |   8  |  9   |   0  |   -    |
+     * |   ESC  |   1  |   2  |   3  |   4  |   5  | PGUP |           | PGDN |   6  |   7  |   8  |  9   |   0  |  PSCR  |
      * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
      * |   TAB  |   Q  |   W  |   E  |   R  |   T  |  [   |           |  ]   |   Y  |   U  |   I  |   O  |   P  |   \    |
      * |--------+------+------+------+------+------|------|           |------|------+------+------+------+------+--------|
@@ -39,7 +39,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         // left hand
         KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_PGUP, KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_LBRC, KC_LCTL, KC_A, KC_S, KC_D, MT(MOD_LALT, KC_F), MT(MOD_LCTL, KC_G), TAB_RO, OSM(MOD_LSFT), KC_Z, KC_X, KC_C, KC_V, KC_B, KC_CAPS, KC_LGUI, TAB_L, TAB_R, TT(_SYMBOLS), KC_LBRC, KC_UP, KC_SPC, KC_DOWN, KC_BSPC,
         // right hand
-        KC_PGDN, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_RBRC, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BSLS, KC_DEL, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, MT(MOD_RGUI, KC_N), KC_M, KC_COMM, KC_DOT, KC_SLSH, OSM(MOD_RSFT), KC_LEFT, KC_UP, KC_DOWN, KC_RGHT, KC_RBRC, KC_ESC, KC_ENT, KC_RGHT, KC_TAB, KC_LEFT),
+        KC_PGDN, KC_6, KC_7, KC_8, KC_9, KC_0, KC_PSCR, KC_RBRC, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BSLS, KC_DEL, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, MT(MOD_RGUI, KC_N), KC_M, KC_COMM, KC_DOT, KC_SLSH, OSM(MOD_RSFT), KC_LEFT, KC_UP, KC_DOWN, KC_RGHT, KC_RBRC, KC_ESC, KC_ENT, KC_RGHT, KC_TAB, KC_LEFT),
 
     /*
      * ,--------------------------------------------------.           ,--------------------------------------------------.
