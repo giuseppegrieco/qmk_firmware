b583086baf31d4a97b27945b9b97ec5e4024d9d7
Giuseppe Grieco <g.grieco1997@gmail.com>
2026-05-25 09:48:21 +0200

    thumb cluster

--- keyboards/handwired/dactyl_manuform/5x7/keymaps/giuseppeg/keymap.c
+++ keyboards/handwired/dactyl_manuform/5x7/keymaps/giuseppeg/keymap.c
@@ -20,7 +20,7 @@
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
  * |   TAB  |   Q  |   W  |   E  |   R  |   T  |  [   |           |  ]   |   Y  |   U  |   I  |   O  |   P  |   \    |
  * |--------+------+------+------+------+------|------|           |------|------+------+------+------+------+--------|
- * |  CTRL  |   A  |   S  |   D  |   F  |G:CTRL| TRO  |           | NUMP |H:CTRL|   J  |   K  |   L  |  ;   |   '    |
+ * |  CTRL  |   A  |   S  |   D  | F:ALT|G:CTRL| TRO  |           | NUMP |H:CTRL| J:ALT|   K  |   L  |  ;   |   '    |
  * |--------+------+------+------+------+------|------'           `------|------+------+------+------+------+--------|
  * |  SHIFT |   Z  |   X  |   C  |   V  |   B  |                         |   N  |   M  |   ,  |   .  |  /   | SHIFT  |
  * `--------+------+------+------+------+-------                         -------+------+------+------+------+--------'
@@ -28,19 +28,19 @@
  *    `-----------------------------'                                              `------------------------------'
  *
  *                                .--------------------.         .---------------.------.
- *                                | SPC  |      |      |         |        |      | BKS  |
- *                                |------| HOME | TASK |         |  CTRL  | PGUP |------|
- *                                | FN   |      |      |         |        |      | ENTR |
+ *                                |   [  |      |      |         |        |      |  ]   |
+ *                                |------|  SPC | BSPC |         |   TAB  | ENTR |------|
+ *                                | FN   |      |      |         |        |      |  ESC |
  *                                '------|-------------|         |---------------'------'
- *                                       | END  | PRNT |         |   ALT  | PGDN |
+ *                                       | UP   | DOWN |         |  LEFT  | RGHT |
  *                                       '------'------'         '--------'------'
  */
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_QWERTY] = LAYOUT_5x7(
         // left hand
-        KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_LBRC, KC_LCTL, KC_A, KC_S, KC_D, KC_F, MT(MOD_LCTL, KC_G), TAB_RO, OSM(MOD_LSFT), KC_Z, KC_X, KC_C, KC_V, KC_B, KC_CAPS, KC_LGUI, TAB_L, TAB_R, TT(_FN), KC_SPC, KC_END, KC_HOME, KC_PSCR, TASK,
+        KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_LBRC, KC_LCTL, KC_A, KC_S, KC_D, MT(MOD_LALT, KC_F), MT(MOD_LCTL, KC_G), TAB_RO, OSM(MOD_LSFT), KC_Z, KC_X, KC_C, KC_V, KC_B, KC_CAPS, KC_LGUI, TAB_L, TAB_R, TT(_FN), KC_LBRC, KC_UP, KC_SPC, KC_DOWN, KC_BSPC,
         // right hand
-        KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, KC_GRV, KC_RBRC, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BSLS, TG(_NUMPAD), MT(MOD_RCTL, KC_H), KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, OSM(MOD_RSFT), KC_LEFT, KC_UP, KC_DOWN, KC_RGHT, KC_BSPC, KC_ENT, KC_PGUP, KC_PGDN, KC_LCTL, KC_LALT),
+        KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, KC_GRV, KC_RBRC, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BSLS, TG(_NUMPAD), MT(MOD_RCTL, KC_H), MT(MOD_RALT, KC_J), KC_K, KC_L, KC_SCLN, KC_QUOT, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, OSM(MOD_RSFT), KC_LEFT, KC_UP, KC_DOWN, KC_RGHT, KC_RBRC, KC_ESC, KC_ENT, KC_RGHT, KC_TAB, KC_LEFT),
 
     [_FN] = LAYOUT_5x7(
         // left hand
