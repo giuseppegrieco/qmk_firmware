5fb2e8fb409da6095ed6a29e4ba923418bf78812
Giuseppe Grieco <g.grieco1997@gmail.com>
2026-05-25 15:26:27 +0200

    finalize symb layout

--- keyboards/handwired/dactyl_manuform/5x7/keymaps/giuseppeg/keymap.c
+++ keyboards/handwired/dactyl_manuform/5x7/keymaps/giuseppeg/keymap.c
@@ -20,7 +20,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
      * |   TAB  |   Q  |   W  |   E  |   R  |   T  |  [   |           |  ]   |   Y  |   U  |   I  |   O  |   P  |   \    |
      * |--------+------+------+------+------+------|------|           |------|------+------+------+------+------+--------|
-     * |  CTRL  |   A  |   S  |   D  | F:ALT|G:CTRL| TRO  |           | DEL  |H:CTRL|   J  |   K  |   L  |  ;   |   '    |
+     * |  CTRL  |   A  |   S  |   D  | F:ALT|G:CTRL| TRO  |           | DEL  |   H  |   J  |   K  |   L  |  ;   |   '    |
      * |--------+------+------+------+------+------|------'           `------|------+------+------+------+------+--------|
      * |  SHIFT |   Z  |   X  |   C  |   V  |   B  |                         |N:SUPR|   M  |   ,  |   .  |  /   | SHIFT  |
      * `--------+------+------+------+------+-------                         -------+------+------+------+------+--------'
@@ -39,7 +39,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         // left hand
         KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_PGUP, KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_LBRC, KC_LCTL, KC_A, KC_S, KC_D, MT(MOD_LALT, KC_F), MT(MOD_LCTL, KC_G), TAB_RO, OSM(MOD_LSFT), KC_Z, KC_X, KC_C, KC_V, KC_B, KC_CAPS, KC_LGUI, TAB_L, TAB_R, TT(_SYMBOLS), KC_LBRC, KC_UP, KC_SPC, KC_DOWN, KC_BSPC,
         // right hand
-        KC_PGDN, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_RBRC, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BSLS, KC_DEL, MT(MOD_RCTL, KC_H), KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, MT(MOD_RGUI, KC_N), KC_M, KC_COMM, KC_DOT, KC_SLSH, OSM(MOD_RSFT), KC_LEFT, KC_UP, KC_DOWN, KC_RGHT, KC_RBRC, KC_ESC, KC_ENT, KC_RGHT, KC_TAB, KC_LEFT),
+        KC_PGDN, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_RBRC, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BSLS, KC_DEL, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, MT(MOD_RGUI, KC_N), KC_M, KC_COMM, KC_DOT, KC_SLSH, OSM(MOD_RSFT), KC_LEFT, KC_UP, KC_DOWN, KC_RGHT, KC_RBRC, KC_ESC, KC_ENT, KC_RGHT, KC_TAB, KC_LEFT),
 
     /*
      * ,--------------------------------------------------.           ,--------------------------------------------------.
@@ -56,8 +56,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      *
      *                                .--------------------.         .---------------.------.
      *                                |      |      |      |         |        |      |      |
-     *                                |------|      |      |         |   '    | SUPR |------|
-     *                                | SYM  |      |      |         |        |      |  -   |
+     *                                |------|      |      |         |   '    |  -   |------|
+     *                                | SYM  |      |      |         |        |      |      |
      *                                '------|-------------|         |---------------'------'
      *                                       |      |      |         |   !    |   "  |
      *                                       '------'------'         '--------'------'
@@ -66,5 +66,5 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         // left hand
         QK_BOOT, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_VOLU, KC_PERC, KC_COMM, KC_LBRC, KC_RBRC, KC_QUES, KC_SCLN, KC_VOLD, KC_PIPE, KC_EQL, KC_LPRN, KC_RPRN, KC_LT, KC_GT, KC_MUTE, KC_HASH, KC_UNDS, KC_LCBR, KC_RCBR, KC_CIRC, KC_MPRV, KC_AT, _______, KC_MSTP, TT(_SYMBOLS), _______, _______, _______, _______, _______,
         // right hand
-        KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, _______, KC_COLN, KC_BSLS, KC_7, KC_8, KC_9, KC_ASTR, _______, KC_AMPR, KC_GRV, KC_4, KC_5, KC_6, KC_PLUS, _______, KC_TILD, KC_1, KC_2, KC_3, KC_BSLS, _______, KC_MPLY, _______, KC_0, KC_MNXT, _______, KC_MINS, KC_RGUI, KC_DQUO, KC_QUOT, KC_EXLM),
+        KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, _______, KC_COLN, KC_BSLS, KC_7, KC_8, KC_9, KC_ASTR, _______, KC_AMPR, KC_GRV, KC_4, KC_5, KC_6, KC_PLUS, _______, KC_TILD, KC_1, KC_2, KC_3, KC_BSLS, _______, KC_MPLY, _______, KC_0, KC_MNXT, _______, _______, KC_MINS, KC_DQUO, KC_QUOT, KC_EXLM),
 };
